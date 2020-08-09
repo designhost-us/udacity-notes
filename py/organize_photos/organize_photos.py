66f2f641e469c07f658a06f683ceb8e7249de2e6
Mike B <designhost.us@gmail.com>
2020-08-09 10:33:51 -0500

    Close Org Project

--- py/organize_photos/organize_photos.py
+++ py/organize_photos/organize_photos.py
@@ -1,1 +1,22 @@
-# organize_photos
+import os
+
+
+def extract_place(filename):
+    return filename.split("_")[1]
+
+
+def organize_photos(directory):
+    os.chdir(directory)
+    originals = os.listdir()
+    index = []
+    for filename in originals:
+        place = extract_place(filename)
+        if place not in index:
+            index.append(place)
+            os.mkdir(place)
+        os.rename(filename, os.path.join(place, filename))
+    print(os.listdir())
+
+
+#  organize_photos("Photos")
+print("Test!")
